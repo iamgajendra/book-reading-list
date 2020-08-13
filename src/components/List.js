3db6a7f42fa9892fb11f1615ead5d860198c0455
Gajendra Singh <gajendrasingh@jklu.edu.in>
2020-08-13 21:37:36 +0530

    first commit

---
+++ src/components/List.js
@@ -0,0 +1,22 @@
+import React, { useContext} from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
+import { BookContext } from '../contexts/BookContext';
+
+const List = () => {
+    const { isLightTheme, light, dark} = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
+    const { books } = useContext(BookContext);
+    return ( 
+        <div className="book-list" style={{color: theme.syntax, background: theme.bg}}>
+            <ul>
+                {books.map(book => {
+                    return (
+                        <li key={book.id} style={{background: theme.ui}}>{ book.title }</li>
+                    );
+                })}
+            </ul>
+        </div> 
+    );
+}
+    
+export default List;
